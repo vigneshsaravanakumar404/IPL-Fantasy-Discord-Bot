6170bb8040c63272c9dfdb5ff6c0d0ef44a9df39
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-02-07 18:09:58 -0500

    Created and Styled Player Rankings Page

--- client/src/pages/Players.js
+++ client/src/pages/Players.js
@@ -1,9 +1,36 @@
+import Unknown from './../images/Unknown.png';
 import './../styles/Players.css';
+
+function Player(props) {
+    var image = "https://scores.iplt20.com/ipl/playerimages/" + props.name.replaceAll(' ', '%20') + '.png';
+
+    const check = new Image();
+    check.src = image;
+    if(!check.complete)
+    {
+        image = Unknown;
+    }
+
+    return(
+        <div class='player-body'>
+            <div class='player-rank-body'>
+                <p class='player-rank'>1</p>
+            </div>
+            <img class='player-image' src={image} alt=''/>
+            <p class='player-name'>{props.name}</p>
+            <p class='player-points'>{props.points}</p>
+        </div>
+    );
+}
 
 function Players() {
     return(
-        <div>
-            <p>This is the players page.</p>
+        <div id='players-content'>
+            <h1>Player Rankings</h1>
+            <Player name={'Virat Kohli'} points={1231}/>
+            <Player name={'Faf Du Plessis'} points={1012}/>
+            <Player name={'Glenn Maxwell'} points={954}/>
+            <Player name={'Mohammed Siraj'} points={912}/>
         </div>
     );
 }
