90fc490c4740ac56af528db92fc400bd20961f17
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-02-08 19:57:03 -0500

    Added and Styled League Page

--- client/src/pages/League.js
+++ client/src/pages/League.js
@@ -1,12 +1,32 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './../styles/League.css';
 
-function League() {
-    let params = useParams();
+function User(props) {
+    const navigate = useNavigate();
+    const params = useParams();
 
     return(
-        <div>
-            <p>This is the league page for {params.league}.</p>
+        <div class='league-user-body' onClick={() => {
+            navigate('/' + params.league + '/' + props.username)
+        }}>
+            <div class='league-user-rank-body'>
+                <p class='league-user-rank'>1</p>
+            </div>
+            <p class='league-user-name'>{props.username}</p>
+            <p class='league-user-points'>{props.points}</p>
+        </div>
+    );
+}
+
+function League() {
+    const params = useParams();
+
+    return(
+        <div id='league-content'>
+            <h1>{params.league}</h1>
+            <User username={'TejasRaghuram'} points={20349}/>
+            <User username={'IshaanSinha'} points={19872}/>
+            <User username={'VigneshSaravanakumar'} points={19428}/>
         </div>
     );
 }
--- client/src/pages/Players.js
+++ client/src/pages/Players.js
@@ -16,22 +16,9 @@ function Player(props) {
             <div class='player-rank-body'>
                 <p class='player-rank'>1</p>
             </div>
-            <img class='player-image' src={image} alt=''/>
+            {!props.mobile && <img class='player-image' src={image} alt=''/>}
             <p class='player-name'>{props.name}</p>
             <p class='player-points'>{props.points}</p>
-        </div>
-    );
-}
-
-function MobilePlayer(props)
-{
-    return(
-        <div class='player-body'>
-            <div class='player-rank-body-mobile'>
-                <p class='player-rank-mobile'>1</p>
-            </div>
-            <p class='player-name-mobile'>{props.name}</p>
-            <p class='player-points-mobile'>{props.points}</p>
         </div>
     );
 }
@@ -43,30 +30,15 @@ function Players() {
         setWidth(window.screen.width)
     }, true);
 
-    if(width <= 768)
-    {
-        return(
-            <div id='players-content'>
-                <h1>Player Rankings</h1>
-                <MobilePlayer name={'Virat Kohli'} points={1231}/>
-                <MobilePlayer name={'Faf Du Plessis'} points={1012}/>
-                <MobilePlayer name={'Glenn Maxwell'} points={954}/>
-                <MobilePlayer name={'Mohammed Siraj'} points={912}/>
-            </div>
-        );
-    }
-    else
-    {
-        return(
-            <div id='players-content'>
-                <h1>Player Rankings</h1>
-                <Player name={'Virat Kohli'} points={1231}/>
-                <Player name={'Faf Du Plessis'} points={1012}/>
-                <Player name={'Glenn Maxwell'} points={954}/>
-                <Player name={'Mohammed Siraj'} points={912}/>
-            </div>
-        );
-    }
+    return(
+        <div id='players-content'>
+            <h1>Player Rankings</h1>
+            <Player name={'Virat Kohli'} points={1231} mobile={width <= 768}/>
+            <Player name={'Faf Du Plessis'} points={1012} mobile={width <= 768}/>
+            <Player name={'Glenn Maxwell'} points={954} mobile={width <= 768}/>
+            <Player name={'Mohammed Siraj'} points={912} mobile={width <= 768}/>
+        </div>
+    );
 }
 
 export default Players;
