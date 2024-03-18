0643e593d3dfe349e9de057118c67ac80a0daa83
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-03-18 17:28:36 -0400

    Working Team Data Extraction
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- backend/Functions.py
+++ backend/Functions.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 from os import system, name
-from Player import Player
-from Team import Team
 from pprint import pprint
+from requests import get
+
+# General Source by team: https://www.espncricinfo.com/records/tournament/averages-batting-bowling-by-team/indian-premier-league-2023-15129?team=4343
+
 
 def format_batter_table(html_data: str):
     """
@@ -23,27 +25,29 @@ def format_batter_table(html_data: str):
     player_data = {}
     for row in table_body.find_all('tr'):
         player_info = [cell.text.strip() for cell in row.find_all('td')]
-        player_name = player_info[0].split('(')[0].strip()  # Extracting player name from the first cell
+        player_name = player_info[0].split('(')[0].strip()
         player_data[player_name] = {
-            "Span": player_info[1],
-            "Mat": int(player_info[2]) if player_info[2] != '-' else player_info[2],
-            "Inns": int(player_info[3]) if player_info[3] != '-' else player_info[3],
-            "NO": int(player_info[4]) if player_info[4] != '-' else player_info[4],
-            "Runs": int(player_info[5]) if player_info[5] != '-' else player_info[5],
+            "Player": player_name,
+            "Mat": int(player_info[1]) if player_info[1] != '-' else player_info[1],
+            "Inns": int(player_info[2]) if player_info[2] != '-' else player_info[2],
+            "NO": int(player_info[3]) if player_info[3] != '-' else player_info[3],
+            "Runs": int(player_info[4]) if player_info[4] != '-' else player_info[4],
+            "BF": int(player_info[5]) if player_info[5] != '-' else player_info[5],
             "HS": player_info[6],
             "Ave": float(player_info[7]) if player_info[7] != '-' else player_info[7],
-            "BF": int(player_info[8]) if player_info[8] != '-' else player_info[8],
-            "SR": float(player_info[9]) if player_info[9] != '-' else player_info[9],
-            "100": int(player_info[10]) if player_info[10] != '-' else player_info[10],
-            "50": int(player_info[11]) if player_info[11] != '-' else player_info[11],
-            "0": int(player_info[12]) if player_info[12] != '-' else player_info[12],
-            "4s": int(player_info[13]) if player_info[13] != '-' else player_info[13],
-            "6s": int(player_info[14]) if player_info[14] != '-' else player_info[14]
+            "SR": float(player_info[8]) if player_info[8] != '-' else player_info[8],
+            "100": int(player_info[9]) if player_info[9] != '-' else player_info[9],
+            "50": int(player_info[10]) if player_info[10] != '-' else player_info[10],
+            "0": int(player_info[11]) if player_info[11] != '-' else player_info[11],
+            "4s": int(player_info[12]) if player_info[12] != '-' else player_info[12],
+            "6s": int(player_info[13]) if player_info[13] != '-' else player_info[13]
         }
 
-    pprint(player_data)
     return player_data
 
+# Missing: Dot Balls, 6s, Hat-Tricks
+# Get 4s from: https://www.espncricinfo.com/records/tournament/bowling-most-4wi-career/indian-premier-league-2023-15129
+# Get 5s from: https://www.espncricinfo.com/records/tournament/bowling-most-5wi-career/indian-premier-league-2023-15129
 def format_bowler_table(html_data: str):
     """
     Parses the HTML data and extracts information from the bowler table.
@@ -73,52 +77,51 @@ def format_bowler_table(html_data: str):
             "Span": player_info[1],
             "Mat": int(player_info[2]) if player_info[2] != '-' else player_info[2],
             "Inns": int(player_info[3]) if player_info[3] != '-' else player_info[3],
-            "Balls": int(player_info[4]) if player_info[4] != '-' else player_info[4],
-            "Overs": float(player_info[5]) if player_info[5] != '-' else player_info[5],
-            "Mdns": int(player_info[6]) if player_info[6] != '-' else player_info[6],
-            "Runs": int(player_info[7]) if player_info[7] != '-' else player_info[7],
-            "Wkts": int(player_info[8]) if player_info[8] != '-' else player_info[8],
-            "BBI": player_info[9],
-            "Ave": float(player_info[10]) if player_info[10] != '-' else player_info[10],
-            "Econ": float(player_info[11]) if player_info[11] != '-' else player_info[11],
-            "SR": float(player_info[12]) if player_info[12] != '-' else player_info[12],
-            "4": int(player_info[13]) if player_info[13] != '-' else player_info[13],
-            "5": int(player_info[14]) if player_info[14] != '-' else player_info[14]
+            "Overs": float(player_info[4]) if player_info[4] != '-' else player_info[4],
+            "Mdns": int(player_info[5]) if player_info[5] != '-' else player_info[5],
+            "Runs": int(player_info[6]) if player_info[6] != '-' else player_info[6],
+            "Wkts": int(player_info[7]) if player_info[7] != '-' else player_info[7],
+            "BBI": player_info[8],
+            "Ave": float(player_info[9]) if player_info[9] != '-' else player_info[9],
+            "Econ": float(player_info[10]) if player_info[10] != '-' else player_info[10],
+            "SR": float(player_info[11]) if player_info[11] != '-' else player_info[11],
+            "5": int(player_info[12]) if player_info[12] != '-' else player_info[12],
+            "10": int(player_info[13]) if player_info[13] != '-' else player_info[13],
+            "Ct": int(player_info[14]) if player_info[14] != '-' else player_info[14],
+            "St": int(player_info[15]) if player_info[15] != '-' else player_info[15]
         }
 
     return player_data
+
+def get_team_data(url: str):
+    """
+    Fetches the batting and bowling data for a team.
+
+    Args:
+        url (str): The URL of the team data.
+
+    Returns:
+        dict: A dictionary containing the batting and bowling data for a team.
+
+    Example:
+        url = "https://www.espncricinfo.com/records/tournament/averages-batting-bowling-by-team/indian-premier-league-2023-15129?team=4343"
+        team_data = get_team_data(url)
+    """
+    response = get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tables = soup.find_all('table')
+    
+    batting_table = tables[0]
+    bowling_table = tables[1]
+
+    return {
+        "batting": format_batter_table(str(batting_table)),
+        "bowling": format_bowler_table(str(bowling_table))
+    }
+
 
 def clear():
     """
     Clears the console screen.
     """
     system('cls' if name == 'nt' else 'clear')
-
-def create_team(batting_data, bowling_data, team_name, player_names):
-    players = []
-    for player in player_names:
-        players.append(Player(team_name, player[0], 
-            batting_data.get(player[0], {}).get("Runs", 0), 
-            batting_data.get(player[0], {}).get("SR", 0), 
-            batting_data.get(player[0], {}).get("4s", 0), 
-            batting_data.get(player[0], {}).get("6s", 0), 
-            batting_data.get(player[0], {}).get("0", 0), 
-            batting_data.get(player[0], {}).get("50", 0), 
-            batting_data.get(player[0], {}).get("100", 0), 
-            batting_data.get(player[0], {}).get("NO", 0), 
-            player[1], 
-            batting_data.get(player[0], {}).get("BF", 0), 
-            bowling_data.get(player[0], {}).get("Wkts", 0), 
-            bowling_data.get(player[0], {}).get("4", 0), 
-            bowling_data.get(player[0], {}).get("5", 0), 
-            player[2], 
-            bowling_data.get(player[0], {}).get("Mdns", 0), 
-            bowling_data.get(player[0], {}).get("hattrick", 0), 
-            bowling_data.get(player[0], {}).get("Econ", 0), 
-            bowling_data.get(player[0], {}).get("Overs", 0), 
-            player[3], 
-            player[4], 
-            player[5], 
-            player[6], 
-            player[7]))
-    return Team(team_name, players)
--- backend/Main.py
+++ backend/Main.py
@@ -1,41 +1,15 @@
 # Imports
-from Functions import format_batter_table, format_bowler_table, clear, create_team
+from Functions import clear, get_team_data
 from requests import get
-from Data import O, W, B, A, S, P
 from flask import *
 
 
 # Variables
-BATTING_DATA_SOURCE = "https://www.espncricinfo.com/records/tournament/averages-batting/indian-premier-league-2023-15129"
-BOWLING_DATA_SOURCE = "https://www.espncricinfo.com/records/tournament/bowling-most-wickets-career/indian-premier-league-2023-15129"
+TEAM_URLS = ["https://www.espncricinfo.com/records/tournament/averages-batting-bowling-by-team/indian-premier-league-2023-15129?team=4343"]
 
-#! Batting Calculations
-print("\033[92mBatting Calculations...\033[0m")
-print("\033[90mRuns, Strike Rate, 0s, 50s, 100s, NOs\033[0m")
-response = get(BATTING_DATA_SOURCE)
-batting_data = format_batter_table(response.text)
-print("\n")
+#! Main
+clear()
 
-
-#! Bowling Calculations
-print("\033[92mBowling Calculations...\033[0m")
-print("\033[90mWickets, Maidens, Hat-Tricks, 4+ Wickets, 5+ Wickets, 6+ Wickets, Economy Rate\033[0m")
-response = get(BOWLING_DATA_SOURCE)
-bowling_data = format_bowler_table(response.text)
-print("\n")
-
-
-#! Create Players and Teams
-print("\033[92mCreate Players and Teams\033[0m")
-teams = []
-teams.append(create_team(batting_data, bowling_data, "Team O", O))
-teams.append(create_team(batting_data, bowling_data, "Team W", W))
-teams.append(create_team(batting_data, bowling_data, "Team B", B))
-teams.append(create_team(batting_data, bowling_data, "Team A", A))
-teams.append(create_team(batting_data, bowling_data, "Team S", S))
-teams.append(create_team(batting_data, bowling_data, "Team P", P))
-
-print("\n")
-
-#! Generate Reports
-print("\033[92mGenerate Reports\033[0m")
+#! Team Data
+for team_url in TEAM_URLS:
+    print(get_team_data(team_url))
