b86ebff71769623eac584e2123d2711ab88f1a78
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-04-11 18:03:43 -0400

    Up to date Data
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Compute.py
+++ Compute.py
@@ -4,55 +4,55 @@ import os
 
 
 output = {}
-template = {
-    "playerID": -1,
-    "batting": {
-        "runs": 0,
-        "4s": 0,
-        "6s": 0,
-        "0s": 0,
-        "50s": 0,
-        "100s": 0,
-        "SR": -1,
-        "BF": 0,
-        "battingAverage": [],
-        "NOs": 0,
-        "HS": 0,
-    },
-    "bowling": {
-        "wickets": 0, 
-        "dots": 0,
-        "economy": -1,
-        "runsConceded": 0,
-        "4H": 0,
-        "5H": 0,
-        "6H": 0,
-        "maiden": 0,
-        "bowlingAverage": [],
-        "bSR": 0,
-        "overs": 0,
-    },
-    "fielding": {
-        "catches": 0,
-        "stumpings": 0
-    },
-    "playerOfTheMatch": 0 #
-}
+matches = []
 
 def addMatch(data):
-    for inning in range(len(data["scoreCard"])):
+    if data.get("scoreCard", []) != []:
+        matches.append(data["scoreCard"][0]["batTeamDetails"]["batTeamName"] + " vs " + data["scoreCard"][0]["bowlTeamDetails"]["bowlTeamName"])
+    for inning in range(len(data.get("scoreCard", []))):
         if data["scoreCard"] != []:
             batsmenData = data["scoreCard"][inning]["batTeamDetails"]
+            print(data["scoreCard"][inning]["batTeamDetails"]["batTeamName"] + " vs " + data["scoreCard"][inning]["bowlTeamDetails"]["bowlTeamName"])
             bowlersData = data["scoreCard"][inning]["bowlTeamDetails"]
-            
+
             for batter in batsmenData["batsmenData"]:
-                
+                batId = int(batsmenData["batsmenData"][batter]["batId"])
+
                 # Add Player if not in output
-                if batsmenData["batsmenData"][batter]["batId"] not in output:
-                    output[batsmenData["batsmenData"][batter]["batId"]] = template
+                if batId not in output:
+                    output[batId] = {
+                        "playerID": -1,
+                        "batting": {
+                            "runs": 0,
+                            "4s": 0,
+                            "6s": 0,
+                            "0s": 0,
+                            "50s": 0,
+                            "100s": 0,
+                            "SR": -1,
+                            "BF": 0,
+                            "battingAverage": -1,
+                            "NOs": 0,
+                            "HS": 0,
+                        },
+                        "bowling": {
+                            "wickets": 0,
+                            "dots": 0,
+                            "economy": -1,
+                            "runsConceded": 0,
+                            "4H": 0,
+                            "5H": 0,
+                            "6H": 0,
+                            "maiden": 0,
+                            "bowlingAverage": -1,
+                            "bSR": 0,
+                            "overs": 0,
+                        },
+                        "fielding": {"catches": 0, "stumpings": 0},
+                        "playerOfTheMatch": 0,  #
+                    }
 
                 # Get Info
-                id = batsmenData["batsmenData"][batter]["batId"]
                 runs = batsmenData["batsmenData"][batter]["runs"]
                 fours = batsmenData["batsmenData"][batter]["fours"]
                 sixes = batsmenData["batsmenData"][batter]["sixes"]
@@ -60,31 +60,81 @@ def addMatch(data):
                 not_out = batsmenData["batsmenData"][batter]["outDesc"] == "not out"
 
                 # Update Info
-                output[id]["playerID"] = id
-                output[id]["batting"]["runs"] += runs
-                output[id]["batting"]["4s"] += fours
-                output[id]["batting"]["6s"] += sixes
-                output[id]["batting"]["BF"] += balls
-                output[id]["batting"]["SR"] = output[id]["batting"]["runs"] / output[id]["batting"]["BF"] * 100
-                output[id]["batting"]["battingAverage"].append(runs)
-                output[id]["batting"]["HS"] = max(output[id]["batting"]["HS"], runs)
+                output[batId]["playerID"] = batId
+                output[batId]["batting"]["runs"] += runs
+                output[batId]["batting"]["4s"] += fours
+                output[batId]["batting"]["6s"] += sixes
+                output[batId]["batting"]["BF"] += balls
+                output[batId]["batting"]["HS"] = max(
+                    output[batId]["batting"]["HS"], runs
+                )
+
+                if output[batId]["batting"]["BF"] == 0:
+                    output[batId]["batting"]["SR"] = 0
+                    output[batId]["batting"]["battingAverage"] = 0
+                else:
+                    output[batId]["batting"]["SR"] = (
+                        output[batId]["batting"]["runs"]
+                        / output[batId]["batting"]["BF"]
+                        * 100
+                    )
+                    output[batId]["batting"]["battingAverage"] = (
+                        output[batId]["batting"]["runs"]
+                        / output[batId]["batting"]["BF"]
+                    )
+
                 if runs >= 100:
-                    output[id]["batting"]["100s"] += 1
+                    output[batId]["batting"]["100s"] += 1
                 elif runs >= 50:
-                    output[id]["batting"]["50s"] += 1
+                    output[batId]["batting"]["50s"] += 1
                 if not_out:
-                    output[id]["batting"]["NOs"] += 1
+                    output[batId]["batting"]["NOs"] += 1
                 elif runs == 0 and balls > 0:
-                    output[id]["batting"]["0s"] += 1
-                
+                    output[batId]["batting"]["0s"] += 1
+
+                if batId == 11813:
+                    print(
+                        f"runs: {runs}, fours: {fours}, sixes: {sixes}, balls: {balls}, not_out: {not_out}"
+                    )
+
             for bowler in bowlersData["bowlersData"]:
+                bowlerId = bowlersData["bowlersData"][bowler]["bowlerId"]
 
                 # Add Player if not in output
-                if bowlersData["bowlersData"][bowler]["bowlerId"] not in output:
-                    output[bowlersData["bowlersData"][bowler]["bowlerId"]] = template
+                if bowlerId not in output:
+                    output[bowlerId] = {
+                        "playerID": -1,
+                        "batting": {
+                            "runs": 0,
+                            "4s": 0,
+                            "6s": 0,
+                            "0s": 0,
+                            "50s": 0,
+                            "100s": 0,
+                            "SR": -1,
+                            "BF": 0,
+                            "battingAverage": -1,
+                            "NOs": 0,
+                            "HS": 0,
+                        },
+                        "bowling": {
+                            "wickets": 0,
+                            "dots": 0,
+                            "economy": -1,
+                            "runsConceded": 0,
+                            "4H": 0,
+                            "5H": 0,
+                            "6H": 0,
+                            "maiden": 0,
+                            "bowlingAverage": -1,
+                            "bSR": 0,
+                            "overs": 0,
+                        },
+                        "fielding": {"catches": 0, "stumpings": 0},
+                        "playerOfTheMatch": 0,  #
+                    }
 
                 # Get Info
-                id = bowlersData["bowlersData"][bowler]["bowlerId"]
                 wickets = bowlersData["bowlersData"][bowler]["wickets"]
                 dots = bowlersData["bowlersData"][bowler]["dots"]
                 runs = bowlersData["bowlersData"][bowler]["runs"]
@@ -92,45 +142,88 @@ def addMatch(data):
                 maidens = bowlersData["bowlersData"][bowler]["maidens"]
 
                 # Update Info
-                output[id]["playerID"] = id
-                output[id]["bowling"]["wickets"] += wickets
-                output[id]["bowling"]["dots"] += dots
-                output[id]["bowling"]["runsConceded"] += runs
-                output[id]["bowling"]["overs"] += balls / 6
-                output[id]["bowling"]["economy"] = output[id]["bowling"]["runsConceded"] / output[id]["bowling"]["overs"]
-                output[id]["bowling"]["bowlingAverage"].append(runs)
-                output[id]["bowling"]["maiden"] += maidens
-                output[id]["bowling"]["bSR"] = output[id]["bowling"]["overs"] * 6 / output[id]["bowling"]["wickets"]
-                if wickets >= 4:
-                    output[id]["bowling"]["4H"] += 1
-                if wickets >= 5:
-                    output[id]["bowling"]["5H"] += 1
-                if wickets >= 6:
-                    output[id]["bowling"]["6H"] += 1
-            
-            # Copare IDs
-            print(id, bowlersData["bowlersData"][bowler]["bowlerId"], batsmenData["batsmenData"][batter]["batId"])
+                output[bowlerId]["playerID"] = bowlerId
+                output[bowlerId]["bowling"]["wickets"] += wickets
+                output[bowlerId]["bowling"]["dots"] += dots
+                output[bowlerId]["bowling"]["runsConceded"] += runs
+                output[bowlerId]["bowling"]["overs"] += balls / 6
+                output[bowlerId]["bowling"]["economy"] = (
+                    output[bowlerId]["bowling"]["runsConceded"]
+                    / output[bowlerId]["bowling"]["overs"]
+                )
+                output[bowlerId]["bowling"]["maiden"] += maidens
 
+                if output[bowlerId]["bowling"]["wickets"] == 0:
+                    output[bowlerId]["bowling"]["bowlingAverage"] = -1
+                    output[bowlerId]["bowling"]["bSR"] = -1
+                else:
+                    output[bowlerId]["bowling"]["bowlingAverage"] = (
+                        output[bowlerId]["bowling"]["runsConceded"]
+                        / output[bowlerId]["bowling"]["wickets"]
+                    )
+                    output[bowlerId]["bowling"]["bSR"] = (
+                        output[bowlerId]["bowling"]["overs"]
+                        / output[bowlerId]["bowling"]["wickets"]
+                    )
+
+                if wickets >= 4:
+                    output[bowlerId]["bowling"]["4H"] += 1
+                if wickets >= 5:
+                    output[bowlerId]["bowling"]["5H"] += 1
+                if wickets >= 6:
+                    output[bowlerId]["bowling"]["6H"] += 1
 
         # Fielding
-        #TODO: Award Catches and Stumpings
+        # TODO: Award Catches and Stumpings
 
         # Player of the Match
-        playerOfTheMatch = data["matchHeader"]["playersOfTheMatch"][0]["id"]
-        if playerOfTheMatch in output:
-            output[playerOfTheMatch]["playerOfTheMatch"] += 1
-        else:
-            output[playerOfTheMatch] = template
-            output[playerOfTheMatch]["playerOfTheMatch"] = 1
+        if data["matchHeader"]["playersOfTheMatch"] != []:
+            playerOfTheMatch = data["matchHeader"]["playersOfTheMatch"][0]["id"]
+            if playerOfTheMatch in output:
+                output[playerOfTheMatch]["playerOfTheMatch"] += 1            
+            else:
+                output[playerOfTheMatch] = {
+                            "playerID": -1,
+                            "batting": {
+                                "runs": 0,
+                                "4s": 0,
+                                "6s": 0,
+                                "0s": 0,
+                                "50s": 0,
+                                "100s": 0,
+                                "SR": -1,
+                                "BF": 0,
+                                "battingAverage": -1,
+                                "NOs": 0,
+                                "HS": 0,
+                            },
+                            "bowling": {
+                                "wickets": 0,
+                                "dots": 0,
+                                "economy": -1,
+                                "runsConceded": 0,
+                                "4H": 0,
+                                "5H": 0,
+                                "6H": 0,
+                                "maiden": 0,
+                                "bowlingAverage": -1,
+                                "bSR": 0,
+                                "overs": 0,
+                            },
+                            "fielding": {"catches": 0, "stumpings": 0},
+                            "playerOfTheMatch": 0,  #
+                        }
+                output[playerOfTheMatch]["playerOfTheMatch"] = 1
+
 
 for file in os.listdir("Data"):
     with open(f"Data/{file}", "r") as f:
         try:
-            print(int(file.split(".")[0]))
             addMatch(json.load(f))
         except ValueError:
-            print(f"Skipping {file}")
+            pass
 
 with open("Data/Output.json", "w") as f:
     json.dump(output, f, indent=2)
 
+print(len(matches))
--- Update.py
+++ Update.py
@@ -1,107 +1,68 @@
+# Missing Matches: Kolkata, Lucknow Super Giants, Sunrisers Hyderabad, Sunrisers Hyderabad, Punjab Kings, Gujarat Titans, Gujarat Titans
+# Missing Matches: 89724
+
 from Constants import MATCHES_URL, SERIES_HEADER
 from requests import get
 from json import dump, load
-from datetime import datetime as Datetime
-import datetime
+from pprint import pprint
+from datetime import datetime
 
-
-
-def getMatchIDs(update=False):
-    """
-    Retrieves the series IDs of matches from the given API endpoint.
-
-    Args:
-        update (bool, optional): Flag indicating whether to update the series data from the API.
-                                Defaults to False.
-
-    Returns:
-        dict: A dictionary containing match IDs as keys and a list of start and end timestamps as values.
-    """
-
-    matchIDS = {}
-
-    if update:
-        series_data = get(MATCHES_URL, headers=SERIES_HEADER).json()
-
-        directory_path = "Data"
-        with open(f"{directory_path}/series.json", "w") as f:
-            dump(series_data, f)
-        print("Series Data Updated")
-
-    else:
-        with open("Data/series.json", "r") as f:
-            series_data = load(f)
-        print("Series Data Loaded")
-
-    for match in series_data["matchDetails"]:
-        try:
-            matchIDS[match["matchDetailsMap"]["match"][0]["matchInfo"]["matchId"]] = [
-                int(match["matchDetailsMap"]["match"][0]["matchInfo"]["startDate"]),
-                int(match["matchDetailsMap"]["match"][0]["matchInfo"]["endDate"])
-                + 3600000,
-            ]
-        except:
-            pass
-
-    return matchIDS
-
-
-# TODO
-def getMatchData(matchID):
-    """
-    Retrieves match data for a given match ID.
-
-    Args:
-        matchID (str): The ID of the match.
-
-    Returns:
-        dict: The match data in JSON format.
-    """
-
-    match_data = get(
+"""
+match_data = get(
         f"https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/{matchID}/hscard",
         headers=SERIES_HEADER,
     )
-    match_data = match_data.json()
+"""
 
-    return match_data
-
-
-# Call This Function Every 5 Minutes
-def UpdateData():
+def updateSeries():
     """
-    Updates the data for IPL matches.
-
-    This function retrieves the match IDs for IPL matches and checks if the current time falls within the match start and end time.
-    If the current time is within the match time range, it fetches the match data and saves it as a JSON file.
-
-    Returns:
-        bool: True if any data was updated, False otherwise.
+    Updates the series data by making an API request to the MATCHES_URL and saving the response to a JSON file.
     """
-    directory_path = "Data"
-    updated = 0
+    series_data = get(MATCHES_URL, headers=SERIES_HEADER).json()    
+    with open(f"Data/series.json", "w") as f:
+        dump(series_data, f)
 
-    if Datetime.now().hour == 23 and Datetime.now().minute >= 55:
-        matchIDS = getMatchIDs(update=True)
-    else:
-        matchIDS = getMatchIDs()
+def updateMatch(matchID):
+    """
+    Updates the match data by making an API request to the MATCHES_URL and saving the response to a JSON file.
+    """
+    match_data = get(
+        f"https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/{matchID}/hscard",
+        headers=SERIES_HEADER,
+    ).json()
+    with open(f"Data/{matchID}.json", "w") as f:
+        dump(match_data, f)
 
-    closest_game = 0
-    for matchID in matchIDS:
-        print(datetime.datetime.fromtimestamp(matchIDS[matchID][0]/1000).strftime('%Y-%m-%d %H:%M:%S') + " to " + datetime.datetime.fromtimestamp(matchIDS[matchID][1]/1000).strftime('%Y-%m-%d %H:%M:%S'))
+# Function is called every 10 minutes during match active times. Then one last time 1 hour after active times
+def UpdateData(updateSeries=False):
+    
+    if updateSeries:
+        updateSeries(update=True)
 
-        if (int(Datetime.now().timestamp()) >= matchIDS[matchID][0] and int(Datetime.now().timestamp()) <= matchIDS[matchID][1]):
+    with open("Data/series.json", "r") as f:
+        series_data = load(f)["matchDetails"]
+        
+    for matchDetailsMap in series_data:
+         if "matchDetailsMap" in matchDetailsMap:
+             for match in matchDetailsMap["matchDetailsMap"]["match"]:
+                 
+                start_time = int(match["matchInfo"]["startDate"])/1000
+                state = match["matchInfo"]["state"]
+                match_id = match["matchInfo"]["matchId"]
 
-            match_data = getMatchData(matchID)
-            with open(f"{directory_path}/{matchID}.json", "w") as f:
-                dump(match_data, f)
-            updated += 1
+                year = datetime.fromtimestamp(start_time).year == datetime.now().year
+                month = datetime.fromtimestamp(start_time).month == datetime.now().month
+                day = datetime.fromtimestamp(start_time).day == datetime.now().day
 
-        if matchIDS[matchID][0] - int(Datetime.now().timestamp()) < closest_game:
-            closest_game = matchIDS[matchID][0] - int(Datetime.now().timestamp())
+                if state == "Complete":
+                    updateMatch(match_id)
+                    print(f"Updated match {match_id}")
 
-    print(f"Closest Game in {closest_game} Seconds")
-    return updated > 0
+    # If the game is today, then update data
+    
+
+    return None
 
 
-print(UpdateData())
+if __name__ == "__main__":
+    UpdateData()
