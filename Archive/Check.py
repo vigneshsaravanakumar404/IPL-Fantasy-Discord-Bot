7b48a3092e36bf91a5bccf6c2bdf33a249bda2dd
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-04-12 08:24:18 -0400

    Parse Team Data
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

---
+++ Archive/Check.py
@@ -0,0 +1,21 @@
+import json
+from pprint import pprint
+
+teams = "Data\Teams.json"
+players = "Data\Players.json"
+
+with open(players, "r") as file:
+    players = json.load(file)
+
+    # get a list of all the values in the dictionary
+    temp = list(players.values())
+
+with open(teams, "r") as file:
+    teams = json.load(file)
+
+    for team in teams:
+        for key in team:
+            if key != "discord":
+                for player in team[key]:
+                    if player not in temp:
+                        print(player) # Varun Chakaravarthy
